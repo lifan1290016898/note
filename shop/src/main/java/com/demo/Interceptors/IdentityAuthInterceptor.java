a48ae1f89a360aa283cd638c1530f39bfbb3e7bf
superLi <1290016898@qq.com>
2020-08-10 16:31:17 +0800

    项目持续开发中

--- shop/src/main/java/com/demo/Interceptors/IdentityAuthInterceptor.java
+++ shop/src/main/java/com/demo/Interceptors/IdentityAuthInterceptor.java
@@ -1,11 +1,14 @@
 package com.demo.Interceptors;
 
+import com.auth0.jwt.interfaces.Claim;
 import com.demo.util.JwtUtil;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
 
 @Component
 public class IdentityAuthInterceptor implements HandlerInterceptor {
@@ -13,6 +16,8 @@ public class IdentityAuthInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         String token = request.getHeader("token");
-        return JwtUtil.verity(token);
+        Map<String, Claim> claimMap = JwtUtil.verifyToken(token);
+        String id = claimMap.get("id").toString();
+        return StringUtils.isNotBlank(id);
     }
 }
